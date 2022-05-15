c59efc8ecb2ff6ddea4a09887927b692efb90e29
ekeith <evanmkeith@gmail.com>
2022-05-14 20:26:18 -0700

    displaying events on tour dates page

--- controllers/ticketMaster.js
+++ controllers/ticketMaster.js
@@ -1,11 +1,16 @@
 const axios = require('axios'); 
 
-const getTourInfo = async(req, res) => {
+const getTourInfo = async(req, response) => {
     const apiKey = process.env.TICKETMASTER_KEY;
-    const keywords = req.body.keyWords;
+    const keyWords = req.body.keyWords;
 
-    await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword=${keyWords}&apikey=${apiKey}`).then((res) => {
-        console.log(res);
+    await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?&keyword=${keyWords}&apikey=${apiKey}`).then((res) => {
+        const events = res.data._embedded.events;
+        return response.status(200).json({
+            status: 200,
+            message: 'Success!',
+            events
+        });
     }).catch((err) => {
         console.log(err);
     });
